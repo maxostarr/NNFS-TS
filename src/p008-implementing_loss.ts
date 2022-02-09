7d513545170bf3f9a33f8ac87c65d678da2406f6
Max Starr <max@maxstarr.xyz>
2022-02-08 19:39:01 -0600

    finish ep8

--- src/p008-implementing_loss.ts
+++ src/p008-implementing_loss.ts
@@ -1,4 +1,5 @@
-import * as tf from "@tensorflow/tfjs-node";
+import * as tf from "@tensorflow/tfjs";
+import { Rank } from "@tensorflow/tfjs";
 import * as math from "mathjs";
 import { plot, Plot } from "nodeplotlib";
 
@@ -43,7 +44,7 @@ class Activation_Softmax {
 
 abstract class Loss {
   calculate(output: tf.Tensor2D, y: tf.Tensor<tf.Rank.R1 | tf.Rank.R2>) {
-    const sample_loss: tf.Scalar = this.forward(output, y);
+    const sample_loss: tf.Tensor = this.forward(output, y);
     const loss: tf.Scalar = sample_loss.mean();
     return loss;
   }
@@ -51,77 +52,89 @@ abstract class Loss {
   abstract forward(
     output: tf.Tensor2D,
     y: tf.Tensor<tf.Rank.R1 | tf.Rank.R2>,
-  ): tf.Scalar;
+  ): tf.Tensor;
 }
 
 class Loss_CategoricalCrossEntropy extends Loss {
   forward(
     y_pred: tf.Tensor2D,
     y_true: tf.Tensor<tf.Rank.R1 | tf.Rank.R2>,
-  ): tf.Scalar {
+  ): tf.Tensor {
     const y_pred_clipped = tf.clipByValue(y_pred, 1e-7, 1.0 - 1e-7);
+    let correct_confidences;
     if (y_true.shape.length === 1) {
-      const correct_confidences = tf.stack(
-        (y_true as unknown as tf.Tensor1D).arraySync().map((v,i)=>y_pred_clipped.slice([i,v],[1,1]).squeeze())
-      )
+      correct_confidences = tf.stack(
+        (y_true as unknown as tf.Tensor1D)
+          .arraySync()
+          .map((v, i) => y_pred_clipped.slice([i, v], [1, 1]).squeeze()),
+      );
     }
     if (y_true.shape.length === 2) {
-      const correct_confidences = tf.sum(y_pred_clipped.mul(y_true), 1);
+      correct_confidences = tf.sum(y_pred_clipped.mul(y_true), 1);
     }
-    return tf.scalar(0);
+    if (!correct_confidences) throw new Error("y_true dims invalid");
+    const loss = tf.log(correct_confidences).neg();
+    return loss;
   }
 }
 
-const output_test = tf.tensor2d([
-  [0.7, 0.1, 0.2],
-  [0.1, 0.5, 0.4],
-  [0.02, 0.9, 0.08],
-]);
+// const output_test = tf.tensor2d([
+//   [0.7, 0.1, 0.2],
+//   [0.1, 0.5, 0.4],
+//   [0.02, 0.9, 0.08],
+// ]);
 
-const y_test = tf.tensor1d([0, 1, 1], "int32");
-const y_test_one_hot = tf.tensor2d([
-  [1, 0, 0],
-  [0, 1, 0],
-  [0, 1, 0],
-]);
+// const y_test = tf.tensor1d([0, 1, 1], "int32");
+// const y_test_one_hot = tf.tensor2d([
+//   [1, 0, 0],
+//   [0, 1, 0],
+//   [0, 1, 0],
+// ]);
 
-const loss_test = new Loss_CategoricalCrossEntropy();
-loss_test.calculate(output_test, y_test);
-loss_test.calculate(output_test, y_test_one_hot);
+// const loss_function = new Loss_CategoricalCrossEntropy();
+// const loss = loss_function.calculate(output_test, y_test_one_hot);
 
-// const POINTS = 100;
-// const CLASSES = 3;
+const POINTS = 3;
+const CLASSES = 3;
 
-// const [X, y] = spiral_data(POINTS, CLASSES);
+const [X, y] = spiral_data(POINTS, CLASSES);
 
-// const data: Plot[] = [
-//   {
-//     x: math
-//       .subset(X, math.index(math.range(0, POINTS * CLASSES), 0))
-//       .toArray()
-//       .flat(),
-//     y: math
-//       .subset(X, math.index(math.range(0, POINTS * CLASSES), 1))
-//       .toArray()
-//       .flat(),
-//     type: "scatter",
-//     mode: "markers",
-//     marker: {
-//       color: y.toArray().flat(),
-//     },
-//   },
-// ];
+const data: Plot[] = [
+  {
+    x: math
+      .subset(X, math.index(math.range(0, POINTS * CLASSES), 0))
+      .toArray()
+      .flat(),
+    y: math
+      .subset(X, math.index(math.range(0, POINTS * CLASSES), 1))
+      .toArray()
+      .flat(),
+    type: "scatter",
+    mode: "markers",
+    marker: {
+      color: y.toArray().flat(),
+    },
+  },
+];
 
-// const dense1 = new Layer_Dense(2, 3);
-// const activation1 = new Activation_ReLU();
+const dense1 = new Layer_Dense(2, 3);
+const activation1 = new Activation_ReLU();
 
-// const dense2 = new Layer_Dense(3, 3);
-// const activation2 = new Activation_Softmax();
+const dense2 = new Layer_Dense(3, 3);
+const activation2 = new Activation_Softmax();
 
-// dense1.forward(tf.tensor2d(X.toArray()));
-// activation1.forward(dense1.output);
+const X_tensor = tf.tensor2d(X.toArray());
+const y_tensor = tf.tensor1d(y.toArray().flat(), "int32");
+y_tensor.print();
+X_tensor.print();
 
-// dense2.forward(activation1.output);
-// activation2.forward(dense2.output);
+dense1.forward(X_tensor);
+activation1.forward(dense1.output);
 
-// console.log(activation2.output.print());
+dense2.forward(activation1.output);
+activation2.forward(dense2.output);
+
+const loss_function = new Loss_CategoricalCrossEntropy();
+const loss = loss_function.calculate(activation2.output, y_tensor);
+
+loss.print();
